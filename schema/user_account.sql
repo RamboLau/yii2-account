7e6d2a79e54aa61e5b7d33e12870f9b7fcb3b648
lubaogui <lbaogui@pku.edu.cn>
2016-01-12 14:31:04 +0800

    Update

--- schema/user_account.sql
+++ schema/user_account.sql
@@ -168,6 +168,6 @@ insert user_account(uid, type, currency, is_enabled) values(1000, 50, 1, 1);
 insert user_account(uid, type, currency, is_enabled) values(10000, 60, 1, 1);
 
 
-alter table `trans_type` AUTO_INCREMENT=100000;
+alter table `trans_type` AUTO_INCREMENT=4;
 alter table `user` AUTO_INCREMENT=100000;
 
