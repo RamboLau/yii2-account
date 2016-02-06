8f409d53c1efd52397c4b61d67aae176ebc347ab
lubaogui <lbaogui@pku.edu.cn>
2016-02-06 10:58:12 +0800

    Update

--- controllers/PayableController.php
+++ controllers/PayableController.php
@@ -241,8 +241,8 @@ class PayableController extends Controller
             [
                 'searchModel'=>$searchModel,
                 'dataProvider'=>$dataProvider,
-            ],
-        )
+            ]
+        );
     }
 
     /**
